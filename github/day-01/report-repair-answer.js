e24bfada58ed1598ef2c69c9ed172a70d8cf732e
Lydia <75341185+Duskstar@users.noreply.github.com>
2020-12-06 22:51:09 +0000

    Finished Day 1!

--- github/day-01/report-repair-answer.js
+++ github/day-01/report-repair-answer.js
@@ -3,7 +3,7 @@ find the two entries that sum to 2020 and then multiply those two numbers togeth
 Of course, your expense report is much larger. Find the two entries that sum to 2020;
 what do you get if you multiply them together?
 */
-function findNumbers(i, j) {
+function findTwoNumbers(i, j) {
   for (i of inputNumbers) {
     for (j of inputNumbers) {
       if (i + j === 2020) {
@@ -14,4 +14,17 @@ function findNumbers(i, j) {
 };
 
 const inputNumbers = require ('/Users/lnicola/Documents/GitHub/my-first-advent-of-code-javascript-2020/github/day-01/input-data.js');
-findNumbers(inputNumbers);
+findTwoNumbers(inputNumbers);
+
+function findThreeNumbers(i, j, k) {
+  for (i of inputNumbers) {
+    for (j of inputNumbers) {
+      for (k of inputNumbers) {
+      if (i + j + k === 2020) {
+        console.log (i * j * k)
+      }
+    }
+  }
+  }
+};
+findThreeNumbers(inputNumbers);
